a4c3cecef780a66038df6b470d5674bba8a48660
NBauschmann <nathalie_bauschmann@hotmail.de>
2024-01-12 15:20:16 +0100

    switching params to dataclasses work in progress

--- launch/top_acoustic_simulation.launch.py
+++ launch/top_acoustic_simulation.launch.py
@@ -1,39 +1,57 @@
 from ament_index_python.packages import get_package_share_path
-from launch import LaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription, GroupAction
-from launch.substitutions import LaunchConfiguration
+from hippo_common.launch_helper import (
+    LaunchArgsDict,
+    declare_vehicle_name_and_sim_time,
+)
+from launch_ros.actions import Node, PushRosNamespace
 
-from launch_ros.actions import PushRosNamespace
-from hippo_common.launch_helper import PassLaunchArguments
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    GroupAction,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 
 def declare_launch_args(launch_description: LaunchDescription):
-    # declare_vehicle_name_and_sim_time(launch_description=launch_description)
-    pass
+    declare_vehicle_name_and_sim_time(launch_description=launch_description)
 
-
-def include_launch_files(launch_description: LaunchDescription):
-    package_path = get_package_share_path('acoustic_simulator')
-
-    path = str(package_path / 'launch/node_sim_anchor_measurements.launch.py')
-    source = PythonLaunchDescriptionSource(path)
-
-    args = PassLaunchArguments()
-    args.add_vehicle_name_and_sim_time()
-
-    tester = IncludeLaunchDescription(source, launch_arguments=args.items())
-
-    action = GroupAction([
-        PushRosNamespace(LaunchConfiguration('vehicle_name')),
-        tester,
-    ])
+    default_acoustic_config_path = get_package_share_path(
+        'acoustic_simulator') / 'config/modem_params.yaml'
+    action = DeclareLaunchArgument(
+        name='acoustic_config_path',
+        default_value=str(default_acoustic_config_path),
+        description='Path to the acoustic simulation configuration .yaml file',
+    )
     launch_description.add_action(action)
+
+
+def create_anchor_sim_node():
+    args = LaunchArgsDict()
+    args.add_vehicle_name_and_sim_time()
+    return Node(
+        package='acoustic_simulator',
+        executable='sim_anchor_measurements_node',
+        parameters=[
+            args,
+            LaunchConfiguration('acoustic_config_path'),
+        ],
+        output='screen',
+        emulate_tty=True,
+    )
 
 
 def generate_launch_description():
     launch_description = LaunchDescription()
     declare_launch_args(launch_description=launch_description)
-    include_launch_files(launch_description=launch_description)
+
+    nodes_group = GroupAction([
+        PushRosNamespace(LaunchConfiguration("vehicle_name")),
+        create_anchor_sim_node()
+    ])
+
+    launch_description.add_action(nodes_group)
 
     return launch_description
