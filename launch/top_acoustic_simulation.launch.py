6b390315932b2d09e4340a7290a3694ba3054f43
NBauschmann <nathalie_bauschmann@hotmail.de>
2024-01-18 13:52:47 +0100

    add robot mesh publisher

--- launch/top_acoustic_simulation.launch.py
+++ launch/top_acoustic_simulation.launch.py
@@ -12,7 +12,7 @@ from launch.actions import (
     GroupAction,
 )
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, TextSubstitution
 
 
 def declare_launch_args(launch_description: LaunchDescription):
@@ -33,7 +33,7 @@ def create_anchor_sim_node():
     args.add_vehicle_name_and_sim_time()
     return Node(
         package='acoustic_simulator',
-        executable='sim_anchor_measurements_node',
+        executable='sim_anchor_measurements_node.py',
         parameters=[
             args,
             LaunchConfiguration('acoustic_config_path'),
@@ -43,13 +43,27 @@ def create_anchor_sim_node():
     )
 
 
+def create_rviz_robot_mesh_publisher():
+    args = LaunchArgsDict()
+    args.add_vehicle_name_and_sim_time()
+    return Node(
+        package='hippo_common',
+        executable='rviz_robot_mesh_publisher',
+        output='screen',
+        parameters=[
+            args,
+        ],
+    )
+
+
 def generate_launch_description():
     launch_description = LaunchDescription()
     declare_launch_args(launch_description=launch_description)
 
     nodes_group = GroupAction([
         PushRosNamespace(LaunchConfiguration("vehicle_name")),
-        create_anchor_sim_node()
+        create_anchor_sim_node(),
+        create_rviz_robot_mesh_publisher()
     ])
 
     launch_description.add_action(nodes_group)
--- nodes/sim_anchor_measurements_node.py
+++ nodes/sim_anchor_measurements_node.py
@@ -3,8 +3,10 @@ import rclpy
 from rclpy.node import Node
 from rcl_interfaces.msg import ParameterDescriptor, ParameterType
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose
 from std_msgs.msg import Header
 from hippo_msgs.msg import ModemOut
+from visualization_msgs.msg import Marker, MarkerArray
 from acoustic_simulator.acoustic_sim_class import acousticSimulation
 from acoustic_simulator.params import read_params_recursion, AnchorParams, AcousticParams, AgentParams, ModemParams, PositionParams
 
@@ -52,6 +54,10 @@ class SimulateAnchorMeasurementsNode(Node):
             pub = self.create_publisher(ModemOut, topic_name, qos_profile=1)
             self.modem_error_publisher_list.append(pub)
 
+        self.marker_pub = self.create_publisher(MarkerArray,
+                                                '~/marker_array',
+                                                qos_profile=1)
+
         self.odometry_sub = self.create_subscription(Odometry,
                                                      'odometry',
                                                      self.on_odometry,
@@ -77,12 +83,19 @@ class SimulateAnchorMeasurementsNode(Node):
             self.get_logger().info(
                 f'Received measurement from modem {measurement["ModemID"]}')
 
+        # publish anchor rviz markers
+        self.publish_rviz_anchors(self.anchors)
+
     def on_odometry(self, msg: Odometry):
         p = msg.pose.pose.position
         v = msg.twist.twist.linear
         with self.lock:
             self.agent_position = [p.x, p.y, p.z]
             self.agent_velocity = [v.x, v.y, v.z]
+
+        # publish bluerov marker
+        # todo: move this somewhere better (e.g. hippo_common)
+        # self.publish_rviz_robot(msg)
 
     def send_measurement(self, id: int, dist: float, t: float):
         msg = ModemOut()
@@ -105,7 +118,7 @@ class SimulateAnchorMeasurementsNode(Node):
 
         self.modem_error_publisher_list[id - 1].publish(msg)
 
-    def fill_header_timestamp(self, t: float):
+    def fill_header_timestamp(self, t: float) -> Header:
         # convert float seconds to int seconds + int nanoseconds
         # to be compatible with time stamp msg type
         header = Header()
@@ -113,7 +126,7 @@ class SimulateAnchorMeasurementsNode(Node):
         header.stamp.nanosec = int((t - int(t)) * 1e9)
         return header
 
-    def init_acoustic_params(self):
+    def init_acoustic_params(self) -> AcousticParams:
         acoustic_params = read_params_recursion(AcousticParams(),
                                                 'acoustic_params', self)
         return acoustic_params
@@ -124,7 +137,7 @@ class SimulateAnchorMeasurementsNode(Node):
             'agent', self)
         return agent_params
 
-    def init_anchor_params(self, anchor_names: list[str]):
+    def init_anchor_params(self, anchor_names: list[str]) -> list[AnchorParams]:
         anchors = []
         for name in anchor_names:
             anchor_params = read_params_recursion(
@@ -132,6 +145,38 @@ class SimulateAnchorMeasurementsNode(Node):
                 name, self)
             anchors.append(anchor_params)
         return anchors
+
+    def publish_rviz_markers(self, markers):
+        self.marker_pub.publish(MarkerArray(markers=markers))
+
+    def publish_rviz_anchors(self, anchors: list[AnchorParams]):
+        markers = []
+        for anchor in anchors:
+            marker = self.create_anchor_marker(anchor)
+            markers.append(marker)
+        self.publish_rviz_markers(markers)
+
+    # def create_robot_markers(self) -> Marker:
+    #     marker = Marker()
+    #     marker.type
+    #     return marker
+
+    def create_anchor_marker(self, anchor: AnchorParams) -> Marker:
+        marker = Marker()
+        marker.type = Marker.CYLINDER
+        marker.id = anchor.modem.id
+        marker.pose = Pose()
+        marker.pose.position.x = anchor.position.x
+        marker.pose.position.y = anchor.position.y
+        marker.pose.position.z = anchor.position.z
+        marker.color.a = 1.0
+        marker.color.r = 1.0
+        marker.scale.x = 0.3
+        marker.scale.y = 0.3
+        marker.scale.z = 0.3
+        marker.header.frame_id = 'map'
+        marker.ns = 'anchors'
+        return marker
 
 
 def main():
